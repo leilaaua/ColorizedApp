1025069a5754b2244f4ded25ebbbd769a0c2b6a6
leilaaua <leila0777@icloud.com>
2021-08-20 19:46:11 +0400

    initial commit

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ColorizedView: UIView!
+    
+    @IBOutlet weak var redCurrentValue: UILabel!
+    @IBOutlet weak var greenCurrentValue: UILabel!
+    @IBOutlet weak var blueCurrentValue: UILabel!
+    
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+
     }
 
-
+    @IBAction func redSliderAction() {
+        redCurrentValue.text = String(redSlider.value)
+        redSlider.minimumTrackTintColor = .red
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenCurrentValue.text = String(greenSlider.value)
+        greenSlider.minimumTrackTintColor = .green
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueCurrentValue.text = String(blueSlider.value)
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    
+    
 }
 
